bda7a540dd78321b4896a903d4622456d48c566e
ana28p <p.anamaria28@gmail.com>
2020-10-20 18:00:46 +0200

    updates

--- changesmining/main.py
+++ changesmining/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import csv
-# from typing import Dict
 from difflib import SequenceMatcher
 from typing import List
 
@@ -42,16 +41,6 @@ def update_modified_file(filename, old_full_path, new_full_path) -> ChangedFile:
         return current_c_file
 
 
-# def search_changed_method_or_create(changed_file: ChangedFile, name: str):
-#     m_key = str(changed_file.full_path) + ':' + name
-#     if m_key in changed_methods:
-#         return changed_methods[m_key]
-#     else:
-#         new_c_met = ChangedMethod(name, changed_file)
-#         changed_methods[m_key] = new_c_met
-#         return new_c_met
-
-
 def split_method_long_name(long_name):
     start_of_name = long_name.rfind('::')
     if start_of_name < 0:
@@ -88,45 +77,75 @@ def update_methods_with_new_class(c_file: ChangedFile, current_path, new_class_p
     for m in c_file.methods:
         if m.class_path == current_path:
             m.class_path = new_class_path
+            break
     c_file.classes.add(new_class_path)
+
 
 def update_method_with_new_class(c_file: ChangedFile, method_name, current_path, new_class_path):
     for m in c_file.methods:
         if (m.class_path == current_path) and (m.name == method_name):
             m.class_path = new_class_path
+            break
     c_file.classes.add(new_class_path)
 
 
-def search_old_method(c_file, m, methods_before):
+def search_and_update_old_method_with_new_class(c_file, m, methods_before):
     sig, class_path = split_method_long_name(m.long_name)
     for old_m in methods_before:
         sig2, old_class_path = split_method_long_name(old_m.long_name)
+        # check the methods in range +-10; in case the file has more classes with the same method signature
         if (sig == sig2) and (m.start_line in range(old_m.start_line - 10, old_m.start_line + 10)):
             update_method_with_new_class(c_file, sig, old_class_path, class_path)
-            return
+            break
+
+
+def get_classes_in_current_file(mod):
+    classes = set()
+    for m in mod.methods:
+        _, class_path = split_method_long_name(m.long_name)
+        classes.add(class_path)
+    return classes
 
 
 def check_class_rename(c_file, mod):
     if len(mod.changed_methods) == 0:
-        # check the current methods if they have a class path not included for this file
         if len(c_file.classes) == 1:
-            # it is enough to check only one method
+            # it is enough to check only one method when there is only one class per file
             _, class_path = split_method_long_name(mod.methods[0].long_name)
             if class_path not in c_file.classes:
                 current_class = c_file.classes.pop()
                 update_methods_with_new_class(c_file, current_class, class_path)
         else:
-            # the methods are in the order they appear in the file (should work if no other change was done)
+            # the methods are in the order they appear in the file
             for i in range(len(mod.methods)):
                 sig, old_class = split_method_long_name(mod.methods_before[i].long_name)
-                _, new_class = split_method_long_name(mod.methods[i].long_name)
-                update_method_with_new_class(c_file, sig, old_class, new_class)
+                sig2, new_class = split_method_long_name(mod.methods[i].long_name)
+                if (sig == sig2) and (old_class != new_class):
+                    c_file.classes.remove(old_class)
+                    update_method_with_new_class(c_file, sig, old_class, new_class)
     else:
-        # current_class_paths = set()
+        all_classes = get_classes_in_current_file(mod)
+        if (len(all_classes) == 1) and (len(c_file.classes) == 1):
+            cls = all_classes.pop()
+            if cls not in c_file.classes:
+                current_class = c_file.classes.pop()
+                update_methods_with_new_class(c_file, current_class, cls)
+        else:
+            raise Exception('Not implemented')
+        # the classes have the form namespace::class_name::
+        renamed_classes = []
+        for cls in c_file.classes:
+            # remove last ::
+            cls_name = cls[:-2]
+            cls_name = cls_name[(cls_name.rfind("::") + 2):]
+            for line_nr, line_val in mod.diff_parsed['deleted']:
+                if (" " + cls_name) in line_val:
+                    renamed_classes.append(cls)
+
         for m in mod.methods:
-            sig, class_path = split_method_long_name(m.long_name)
-            # current_class_paths.add(class_path)
-            search_old_method(c_file, m, mod.methods_before)
+            _, class_path = split_method_long_name(m.long_name)
+            if class_path in renamed_classes:
+                search_and_update_old_method_with_new_class(c_file, m, mod.methods_before)
 
 
 def remove_methods(c_file, obsolete_methods):
@@ -136,20 +155,12 @@ def remove_methods(c_file, obsolete_methods):
 
 
 def replace_method(c_file, commit, old_long_name, new_long_name):
-    # if commit.commit_hash == '03ca03d601d7f2c206e5e776d67155b0866afc70':
-    #     print()
     matches = [m for m in c_file.methods if (m.class_path + m.name).replace(' ', '') == old_long_name.replace(' ', '')]
     if len(matches) == 0:
-        # maybe the class was renamed already; search with the new class too
         sig, _ = split_method_long_name(old_long_name)
-        _, class_path = split_method_long_name(new_long_name)
-        if commit.commit_hash == '413851a1425c2caf0600330d6ee1aaa2b63b90b3':
-            print((class_path + sig))
-            print([(m.class_path + m.name) for m in c_file.methods])
-        matches = [m for m in c_file.methods if (m.class_path + m.name).replace(' ', '') == (class_path + sig).replace(' ', '')]
-        if len(matches) == 0:
-            # last chance search for a method with the same name (if it's overloaded will fail)
-            matches = [m for m in c_file.methods if m.name[:m.name.rfind('(')] == sig[:sig.rfind('(')]]
+        # try to search for a method with the same name (if it's overloaded will fail)
+        # can be the case that two different commits change the method signature in different ways
+        matches = [m for m in c_file.methods if m.name[:m.name.rfind('(')] == sig[:sig.rfind('(')]]
 
     if len(matches) == 1:
         signature, class_path = split_method_long_name(new_long_name)
@@ -157,7 +168,8 @@ def replace_method(c_file, commit, old_long_name, new_long_name):
         matches[0].class_path = class_path
         matches[0].commits.append(commit)
     else:
-        raise Exception('The method was not found (should be present) or too many matches', matches, old_long_name, commit.commit_hash, c_file.full_path)
+        raise ValueError('The method was not found (should be present) or too many matches',
+                         [(m.class_path + m.name) for m in matches], old_long_name, commit.commit_hash, c_file.full_path)
 
 
 def check_and_update_methods(c_file, commit, modification):
@@ -176,17 +188,23 @@ def check_and_update_methods(c_file, commit, modification):
         else:
             updated_obs = []
             for om in obsolete_methods:
-                max_sim = ['', '', 0]
+                max_similarity = ['', '', 0]
                 for nm in new_methods:
                     sig_m1, _ = split_method_long_name(om)
                     sig_m2, _ = split_method_long_name(nm)
                     sim = SequenceMatcher(None, sig_m1, sig_m2).ratio()
-                    if sim > max_sim[2]:
-                        max_sim = [om, nm, sim]
-                if max_sim[2] >= 0.75:
-                    replace_method(c_file, commit, max_sim[0], max_sim[1])
-                    updated_obs.append(max_sim[0])
-                    new_methods.remove(max_sim[1])
+                    if sim > max_similarity[2]:
+                        max_similarity = [om, nm, sim]
+                if max_similarity[2] >= 0.6:
+                    try:
+                        replace_method(c_file, commit, max_similarity[0], max_similarity[1])
+                        updated_obs.append(max_similarity[0])
+                        new_methods.remove(max_similarity[1])
+                    except ValueError as e:
+                        # if the replace fails, don't do anything
+                        # the old method will be removed and the new method will be considered as new
+                        print(e)
+            # check if all obsolete methods were updated
             if len(updated_obs) != len(obsolete_methods):
                 to_remove = [val for val in obsolete_methods if val not in updated_obs]
                 remove_methods(c_file, to_remove)
@@ -207,18 +225,25 @@ def print_actual_files():
             print(mp.class_path + mp.name, ' nr of commits: ', len(mp.commits))
 
 
-def write_to_csv(list):
-    with open('C:/Users/aprodea/work/metrics-tax-compare/commits/commits_tax_compare.csv', 'w') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=';')
-        filewriter.writerow(['Full_path', 'Filename', 'Method', 'Changes'])
-        for key, v_f in list.items():
+def write_to_csv(dict_files):
+    with open('C:/Users/aprodea/work/testing/commits.csv', 'w') as csvfile:
+        fieldnames = ['Full_path', 'Filename', 'Method', 'Changes']
+        file_writer = csv.DictWriter(csvfile, fieldnames, delimiter=';', lineterminator='\n')
+        file_writer.writeheader()
+        for key, v_f in dict_files.items():
             for mp in v_f.methods:
                 method_full_name = (mp.class_path + mp.name)
-                filewriter.writerow([v_f.full_path, v_f.filename, method_full_name, len(mp.commits)])
+                file_writer.writerow({
+                    'Full_path': v_f.full_path,
+                    'Filename': v_f.filename,
+                    'Method': method_full_name,
+                    'Changes': len(mp.commits)
+                })
 
 
 def mine(repo):
     # for c in RepositoryMining('C:/Users/aprodea/work/deloitte-tax-compare/.git').traverse_commits():
+    # for c in RepositoryMining(repo, single='fea5cc2adb4838c2f005042c5ace1fbca9c43614', only_modifications_with_file_types=['.cs']).traverse_commits():
     for c in RepositoryMining(repo, only_modifications_with_file_types=['.cs']).traverse_commits():
         # print('--->', c.msg)
         commit = Commit(c.committer_date, c.committer, c.msg, c.hash)
@@ -226,7 +251,6 @@ def mine(repo):
         # print(c.modifications)
         for mod in c.modifications:
             if mod.filename.endswith('.cs'):
-                # print(mod.change_type, mod.filename, c.msg, c.hash)
                 if mod.change_type == ModificationType.ADD:
                     # add all methods for file
                     c_file = search_modified_file_or_create(mod.filename, mod.new_path)
@@ -235,10 +259,7 @@ def mine(repo):
                     # delete file (and its methods)
                     files.pop(mod.old_path)
                 else:
-                    # print('not add or delete')
-                    # print(mod.change_type == ModificationType.RENAME)
                     if mod.change_type == ModificationType.RENAME:
-                        # print('rename', mod.filename, mod.old_path, mod.new_path)
                         c_file = update_modified_file(mod.filename, mod.old_path, mod.new_path)
                     else:
                         c_file = search_modified_file_or_create(mod.filename, mod.new_path)
@@ -252,8 +273,8 @@ def mine(repo):
         # print_actual_files()
 
 
-mine('C:/Users/aprodea/work/deloitte-tax-compare/.git')
-# mine('https://github.com/ana28p/testing-with-csharp.git')
+# mine('C:/Users/aprodea/work/deloitte-tax-compare/.git')
+mine('https://github.com/ana28p/testing-with-csharp.git')
 # for k, m in changed_methods.items():
 #     print(k, ' name: ', m.name, ' modifications:', len(m.modifications))
 write_to_csv(files)
---
+++ datareading/rscripts.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: aprodea
+# Created on: 07-10-2020
+
